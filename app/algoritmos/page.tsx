5855b517908e0dca9e953d0a799f3d4b879a8d6e
Beak <chenyilong916002@gmail.com>
2025-09-16 01:06:08 +0800

    first commit

---
+++ app/algoritmos/page.tsx
@@ -0,0 +1,5 @@
+export default function AlgoritmosPage() {
+  return <div>
+    我的算法学习方法论
+  </div>
+}
---
+++ data/headerNavLinks.ts
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/algoritmos', title: 'Algoritmos' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
